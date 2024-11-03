938fad2c931b80d6538d3acdbebdb56b7fc4dc94
jeffreyd3308 <jeffreyd3308@nycstudents.net>
2024-11-03 18:41:48 +0000

    commit

--- U4T1_Lab2/Main.java
+++ U4T1_Lab2/Main.java
@@ -3,20 +3,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What number do you want to factorial? ");
-        System.out.print("Enter a number between 0 and 170: ");
+        System.out.println("Enter a number that is either positive, even, and a multiple of 7 OR prime:");
         int userNumber = scanner.nextInt();
-        double answer = 1.0;
         
-        while (userNumber <= 0 || userNumber > 170) {
-            System.out.print("No! Between 0 and 170: ");
+        while (!(userNumber >= 0 && userNumber % 2 == 0 && userNumber % 7 == 0) && !(isPrime(userNumber))) {
+            if (!(userNumber >= 0)) {
+                System.out.println("Not positive");
+            }
+            if (!(userNumber % 2 == 0)) {
+                System.out.println("Not even");
+            }
+            if (!(userNumber % 7 == 0)) {
+                System.out.println("Not multiple of 7");
+            }
+            System.out.print("Try again: ");
             userNumber = scanner.nextInt();
         }
 
-        for (int i = 1; i <= userNumber; i++) {
-            answer *= i;
+        if (isPrime(userNumber)) {
+            System.out.println("Well done, " + userNumber + " is prime!");
+        } else {
+            System.out.println("Well done, " + userNumber + " is a positive, even, multiple of 7!");
         }
 
-        System.out.println(userNumber + "! is " + answer);
+    }
+
+    /* Returns true if number is prime and false
+            if it is not prime; if number is 1, return false */
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
+        return false;
+        }
+
+        int currentDivisor = 2; //start at 2 since no need to check 1
+
+        while (currentDivisor < number) {
+        if (number % currentDivisor == 0) {
+            return false;  // breaks the loop AND exits the method
+        }
+        currentDivisor++;
+        }
+        return true;
     }
 }
+
